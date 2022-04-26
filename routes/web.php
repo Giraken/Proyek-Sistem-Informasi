b9a44633818d20ff9d917042db9e804de115b8fb
envrico17 <enrico.indra.2005356@students.um.ac.id>
2022-04-26 21:38:28 +0700

    View Cari Lomba
    asefawef

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,6 @@ Route::get('/edit_profil', function() {
 Route::get('/publikasi', function() {
     return view('publikasi');
 });
+Route::get('/cari_lomba', function() {
+    return view('cari_lomba');
+});
